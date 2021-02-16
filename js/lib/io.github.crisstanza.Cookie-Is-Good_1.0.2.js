ee74a7ece3ff9b4e5cd61afabaa8aa78f77ae787
Cris Stanza <crisstanza@users.noreply.github.com>
2021-02-16 15:59:18 -0200

    Fix for non-string cookie value; using let instead of var.

--- js/lib/io.github.crisstanza.Cookie-Is-Good_1.0.2.js
+++ js/lib/io.github.crisstanza.Cookie-Is-Good_1.0.2.js
@@ -9,7 +9,7 @@ if (!io.github.crisstanza.CookieIsGood) io.github.crisstanza.CookieIsGood = {};
 	io.github.crisstanza.CookieIsGood.NOT_FOUND = -1;
 
 	io.github.crisstanza.CookieIsGood.del = function(cookieName, path) {
-		var result = io.github.crisstanza.CookieIsGood.get(cookieName, path);
+		let result = io.github.crisstanza.CookieIsGood.get(cookieName, path);
 		if (result === io.github.crisstanza.CookieIsGood.NOT_FOUND) {
 			return result;
 		} else {
@@ -18,14 +18,14 @@ if (!io.github.crisstanza.CookieIsGood) io.github.crisstanza.CookieIsGood = {};
 	};
 
 	io.github.crisstanza.CookieIsGood.get = function(cookieName) {
-		var cookieString = document.cookie;
-		var cookies = cookieString.split(/\s*;\s*/);
-		var cookiesLength = cookies.length;
-		for (var i = 0; i < cookiesLength; i++) {
-			var cookie = cookies[i];
-			var cookieNameValue = cookie.split(/\s*=\s*/);
+		let cookieString = document.cookie;
+		let cookies = cookieString.split(/\s*;\s*/);
+		let cookiesLength = cookies.length;
+		for (let i = 0; i < cookiesLength; i++) {
+			let cookie = cookies[i];
+			let cookieNameValue = cookie.split(/\s*=\s*/);
 			if (cookieNameValue[0] == cookieName) {
-				var cookieValue  = decodeURIComponent(cookieNameValue[1]);
+				let cookieValue  = decodeURIComponent(cookieNameValue[1]);
 				cookieValue = cookieValue.replace(new RegExp('%28', 'g'), '(');
 				cookieValue = cookieValue.replace(new RegExp('%29', 'g'), ')');
 				return cookieValue;
@@ -35,13 +35,16 @@ if (!io.github.crisstanza.CookieIsGood) io.github.crisstanza.CookieIsGood = {};
 	};
 
 	io.github.crisstanza.CookieIsGood.set = function(cookieName, cookieValueOrJson, path, expirationInDays) {
+		let expiration;
 		if (expirationInDays) {
-			var expiration = new Date();
+			expiration = new Date();
 			expiration.setTime(expiration.getTime() + expirationInDays*24*60*60*1000);
 		}
-		var cookieValue;
+		let cookieValue;
 		if (typeof cookieValueOrJson == 'object') {
 			cookieValue = JSON.stringify(cookieValueOrJson);
+		} else if (typeof cookieValueOrJson != 'string') {
+			cookieValue = new String(cookieValueOrJson);
 		} else {
 			cookieValue = cookieValueOrJson;
 		}
